de86f04b0a887a2e37d3f7c01f866d946e590009
Ailebiz <bolkyzyaliza@gmail.com>
2024-11-13 22:49:51 +0500

    қатемді түзеттім

--- day-1/js/loops.js
+++ day-1/js/loops.js
@@ -20,7 +20,7 @@ let number = Number(prompt("Кез келген сан енгізіңіз:"));
 for (let i = 1; i <= number; i++) {
     
     if (i % 3 === 0 || i % 5 === 0) {
-        console.log(i);
+        console.log( i );
     }
   
     if (i % 3 === 0 || i % 5 === 0 && i % 2 === 0) {
